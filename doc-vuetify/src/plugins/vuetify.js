5a40b65cbab2cc19cddf583f711abbf890ace44d
Vagner Barros <vagnerbarrospereira@gmail.com>
2020-03-02 15:56:07 -0300

    Componente calendario pronto

--- doc-vuetify/src/plugins/vuetify.js
+++ doc-vuetify/src/plugins/vuetify.js
@@ -1,7 +1,28 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
+import pt from 'vuetify/es5/locale/pt';
+import colors from 'vuetify/lib/util/colors';
+import 'vuetify/dist/vuetify.min.css';
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  lang: {
+    locales: { pt },
+    current: 'pt'
+  },
+  theme: {
+    dark: false,
+    themes: {
+      light: {
+        primary: colors.blue.base,
+        secondary: colors.blue.lighten4,
+        accent: colors.blue.base
+      }
+    },
+  },
+  icons: {
+    iconfont: 'mdi',
+  },
 });
--- doc-vuetify/src/views/Calendar.vue
+++ doc-vuetify/src/views/Calendar.vue
@@ -4,7 +4,7 @@
       <v-col>
         <v-sheet height="80">
           <v-toolbar text dark color="primary">
-            <v-toolbar-title >Calendário {{ title}}</v-toolbar-title>
+            <v-toolbar-title >{{ title}}</v-toolbar-title>
             <v-spacer></v-spacer>
             <v-menu bottom right class="mr-3">
               <template v-slot:activator="{ on }">
@@ -33,7 +33,7 @@
           </v-toolbar>
         </v-sheet>
         <v-sheet heigth="600">
-          <v-calendar ref="calendar" v-model="focus">
+          <v-calendar ref="calendar" v-model="focus" :type="type" @click:date="viewDay" @change="updateRange" :event-color="getEventColor" :events="events">
           </v-calendar>
         </v-sheet>
       </v-col>
@@ -42,12 +42,14 @@
 </template>
 
 <script>
+import moment from 'moment';
+moment.locale('pt-BR');
 
 export default {
   name: 'Calendar',
   data: () => ({
     focus: '',
-    type: 'Week',
+    type: 'week',
     typeToLabel: {
       month: 'Month',
       week: 'Week',
@@ -60,7 +62,8 @@ export default {
     selectedElement: null,
     selectedOpen: false,
     events: [],
-
+    colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
+    names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
   }),
 
   computed: {
@@ -72,42 +75,84 @@ export default {
         return '';
       }
 
-      const startMonth = this.monthFormatter(start)
-      const endMonth = this.monthFormatter(end)
-      const suffixMonth = startMonth === endMonth ? '' : endMonth
-      const startYear = start.year
-      const endYear = end.year
-      const suffixYear = startYear === endYear ? '' : endYear
-      const startDay = start.day + this.nth(start.day)
-      const endDay = end.day + this.nth(end.day)
+      const startMonth = this.monthFormatter(start);
+      const endMonth = this.monthFormatter(end);
+      const suffixMonth = startMonth === endMonth ? '' : endMonth;
+      const startYear = start.year;
+      const endYear = end.year;
+      const suffixYear = startYear === endYear ? '' : endYear;
+      const startDay = start.day;
+      const endDay = end.day;
       switch (this.type) {
         case 'month':
-          return `${startMonth} ${startYear}`
+          return `${startMonth} ${startYear}`;
         case 'week':
+          return `${startDay} - ${endDay} de ${startMonth} ${startYear}`;
         case 'day':
-          return `${startMonth} ${startDay} ${startYear}`
+          return `${startDay} de ${startMonth} ${startYear}`;
       }
-      return ''
+      return '';
     },
     
-    monthFormatter () {
-      return this.$refs.calendar.getFormatter({
-        timeZone: 'UTC', month: 'long',
-      })
-    },
+    
   },
 
   methods: {
 
-    setHoje(){
+    monthFormatter(data) {
+      return moment(data).format('MMMM');
+    },
 
+    viewDay ({ date }) {
+      this.focus = date;
+      this.type = 'day';
+    },
+    setHoje(){
+      this.focus = this.today;
+    },
+    getEventColor (event) {
+      return event.color
     },
     voltar(){
-
+      this.$refs.calendar.prev();
     },
     avancar(){
+      this.$refs.calendar.next();
+    },
 
-    }
+    updateRange ({ start, end }) {
+      const events = [];
+      const min = new Date(`${start.date}T00:00:00`);
+      const max = new Date(`${end.date}T23:59:59`);
+      const days = (max.getTime() - min.getTime()) / 86400000;
+      const eventCount = this.rnd(days, days + 20);
+      for (let i = 0; i < eventCount; i++) {
+        const allDay = this.rnd(0, 3) === 0;
+        const firstTimestamp = this.rnd(min.getTime(), max.getTime());
+        const first = new Date(firstTimestamp - (firstTimestamp % 900000));
+        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
+        const second = new Date(first.getTime() + secondTimestamp);
+        events.push({
+          name: this.names[this.rnd(0, this.names.length - 1)],
+          start: this.formatDate(first, !allDay),
+          end: this.formatDate(second, !allDay),
+          color: this.colors[this.rnd(0, this.colors.length - 1)],
+        });
+      }
+      this.start = start;
+      this.end = end;
+      this.events = events;
+    },
+
+    rnd (a, b) {
+      return Math.floor((b - a + 1) * Math.random()) + a
+    },
+
+    formatDate (a, withTime) {
+      return withTime
+        ? `${a.getFullYear()}-${a.getMonth() + 1}-${a.getDate()} ${a.getHours()}:${a.getMinutes()}`
+        : `${a.getFullYear()}-${a.getMonth() + 1}-${a.getDate()}`
+    },
   },
 
   mounted () {
